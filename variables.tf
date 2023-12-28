41243532e51ca7f745fef9d0e9a0fd5e62cd30eb
tsukky-empower <97720783+tsukky-empower@users.noreply.github.com>
2023-12-28 16:12:46 +0900

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
+variable "location" {}
+variable "admin_password" {}
+variable "hello_tf_instance_count" {
+    default = 1
+}
